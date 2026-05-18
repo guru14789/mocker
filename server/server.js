da0e836762304230c43f0bdef043160954071e56
Kishore B <kishore3014b@gmail.com>
2026-05-18 09:09:14 +0530

    refactor(server): remove legacy MongoDB references

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const app = require('./app');
 const { db } = require('./firebase.admin');
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/software-mocker';
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} (Firebase Mode)`);
